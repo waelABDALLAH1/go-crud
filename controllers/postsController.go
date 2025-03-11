bf091ea2e5253873cb5466957d64fd5454febc52
waelabdallah <waelabdallah@callem.ai>
2025-03-12 00:06:36 +0100

    adding first api

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -1,10 +1,24 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-crud/initializers"
+	"go-crud/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PostsCreate(c *gin.Context) {
-	
+
+	post := models.Post{Title: "Hello", Body: "the body "}
+	result := initializers.DB.Create(&post)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"error": result.Error,
+		})
+		return
+	}	
+
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"post": post,
 	})
 }
