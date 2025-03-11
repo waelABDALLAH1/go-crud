d6e77e3885504f85e334b481de9098f6a1df3e15
waelabdallah <waelabdallah@callem.ai>
2025-03-12 00:11:11 +0100

    add parsing data

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -8,15 +8,21 @@ import (
 )
 
 func PostsCreate(c *gin.Context) {
+	var body struct {
+		Title string
+		Body  string
+	}
 
-	post := models.Post{Title: "Hello", Body: "the body "}
+	c.Bind(&body)
+
+	post := models.Post{Title: body.Title, Body: body.Body}
 	result := initializers.DB.Create(&post)
 	if result.Error != nil {
 		c.JSON(400, gin.H{
 			"error": result.Error,
 		})
 		return
-	}	
+	}
 
 	c.JSON(200, gin.H{
 		"post": post,
